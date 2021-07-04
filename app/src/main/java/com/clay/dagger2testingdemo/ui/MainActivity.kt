ddf7d95756b3d9fe07323d9958af9431af805780
Clay-CL <clay.motupalli@gmail.com>
2021-07-04 20:29:09 +0530

    null binding bug in MainActivity fixed

--- app/src/main/java/com/clay/dagger2testingdemo/ui/MainActivity.kt
+++ app/src/main/java/com/clay/dagger2testingdemo/ui/MainActivity.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
-    private val binding = _binding!!
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
