005b6e485acd10680d5937d895c5fbae664d1ad5
Clay-FloMobility <clay@flomobility.com>
2021-07-04 20:07:47 +0530

    removed .idea from repo and added viewBinding to MainActivity

--- app/src/main/java/com/clay/dagger2testingdemo/di/RoomModule.kt
+++ app/src/main/java/com/clay/dagger2testingdemo/di/RoomModule.kt
@@ -1,14 +1,9 @@
 package com.clay.dagger2testingdemo.di
 
-import android.app.Application
 import android.content.Context
 import androidx.room.Room
-import com.clay.dagger2testingdemo.data.local.WordDao
 import com.clay.dagger2testingdemo.data.local.WordDatabase
 import com.clay.dagger2testingdemo.other.Constants.DATABASE_NAME
-import com.clay.dagger2testingdemo.repositories.DefaultWordRepository
-import com.clay.dagger2testingdemo.repositories.WordRepository
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
--- app/src/main/java/com/clay/dagger2testingdemo/ui/MainActivity.kt
+++ app/src/main/java/com/clay/dagger2testingdemo/ui/MainActivity.kt
@@ -3,12 +3,24 @@ package com.clay.dagger2testingdemo.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.clay.dagger2testingdemo.R
+import com.clay.dagger2testingdemo.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
 }
